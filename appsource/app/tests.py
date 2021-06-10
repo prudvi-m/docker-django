bedfd52bae081a36a70727d2ec1cc6f789ccf609
prudvi-m <69066646+prudvi-m@users.noreply.github.com>
2021-06-10 19:47:50 +0530

    sample code for TDD

--- appsource/app/tests.py
+++ appsource/app/tests.py
@@ -1,8 +1,13 @@
 from django.test import TestCase
-from .calc import add
+from .calc import add,subtract
 
 class sampleTest(TestCase):
     
+    
     def test_add_numbers(self):
-        self.assertEqual(add(10,11),21)
+        
+        self.assertEqual(add(10,11), 21)
+        
+    def test_subtract_numbers(self):
+        self.assertEqual(subtract(20,10),10)
         
